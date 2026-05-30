c9da15784421ff7394240d7c081d161e7752013d
hammad-1k <hammadmustafa1k@gmail.com>
2026-05-30 05:43:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -100,5 +100,4 @@ def parse():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
